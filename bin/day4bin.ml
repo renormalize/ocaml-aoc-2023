49354f9affdc9f8a119778bf4cc0b13d321a9c06
renormalize <51327242+renormalize@users.noreply.github.com>
2024-01-12 01:28:32 +0530

    Day 4, Part 1 solutions

---
+++ bin/day4bin.ml
@@ -0,0 +1,5 @@
+let () = Printf.printf "\nSolving Day 4!\n"
+
+let () =
+  Printf.printf "The sum of the points is: %d\n\n"
+    (Day4.total_points "day4/input.txt")
---
+++ day4/day4.ml
@@ -0,0 +1,38 @@
+let lines file = In_channel.input_lines (In_channel.open_text file)
+
+let parse_ints s =
+  String.split_on_char ' ' s
+  |> List.filter (fun s -> not (String.equal s ""))
+  |> List.map (fun s -> int_of_string s)
+
+let winning_and_had card_string =
+  let both_number_sets =
+    match String.split_on_char ':' card_string with
+    | [ _; t ] -> t
+    | _ -> failwith "Unexpected input!"
+  in
+  let winning, had =
+    match String.split_on_char '|' both_number_sets with
+    | [ h; t ] -> (String.trim h, String.trim t)
+    | _ -> failwith "Unexpected input!"
+  in
+  (parse_ints winning, parse_ints had)
+
+let pow a n =
+  let rec pow_rec = function
+    | 0 -> 1
+    | 1 -> a
+    | n ->
+        let b = pow_rec (n / 2) in
+        b * b * if n mod 2 = 0 then 1 else a
+  in
+  if n >= 0 then pow_rec n else 0
+
+let no_cards_present (winning, had) =
+  List.filter (fun card -> List.mem card winning) had |> List.length
+
+let score_card card_string =
+  pow 2 (no_cards_present (winning_and_had card_string) - 1)
+
+let total_points file =
+  List.fold_left ( + ) 0 (List.map score_card (lines file))
