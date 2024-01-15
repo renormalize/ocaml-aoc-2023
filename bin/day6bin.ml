44604479230928ef885fb9f4e805cafe2c09ecd8
renormalize <51327242+renormalize@users.noreply.github.com>
2024-01-15 12:59:47 +0530

    Solutions to both parts of day 6

--- bin/day6bin.ml
+++ bin/day6bin.ml
@@ -2,5 +2,11 @@ let () = Printf.printf "\nSolving Day 6!\n"
 
 let () =
   Printf.printf
-    "The product of the number of possibilities with spaces considered is: %d\n\n"
+    "The product of the number of possibilities with spaces considered is: %d\n"
     (Day6.solve_string_with_spaces "day6/input.txt")
+
+let () =
+  Printf.printf
+    "The product of the number of possibilities with spaces not considered is: \
+     %d\n\n"
+    (Day6.solve_string_without_spaces "day6/input.txt")
--- day6/day6.ml
+++ day6/day6.ml
@@ -5,6 +5,7 @@ let roots_quadratic_sanitized a b c =
   ( ceil ((-.b -. sqrt ((b *. b) -. (4. *. a *. c))) /. (2. *. a)),
     floor ((-.b +. sqrt ((b *. b) -. (4. *. a *. c))) /. (2. *. a)) )
 
+(*extracts the digits from the input strings*)
 let extract_digits line =
   List.map
     (fun x -> float_of_int (int_of_string x))
@@ -13,11 +14,13 @@ let extract_digits line =
           (fun x -> not (String.equal "" x))
           (String.split_on_char ' ' line)))
 
+(*returns the no of possibilites given the time and the distance to beat*)
 let no_possibilities time distance =
   let minus_one = -1. in
   let roots = roots_quadratic_sanitized minus_one time (-.distance) in
   int_of_float (fst roots) - int_of_float (snd roots) - 1
 
+(*solution with the spaces in the input considered*)
 let solve_string_with_spaces file =
   let time_and_distance = lines file in
   let times = extract_digits (List.hd time_and_distance) in
@@ -28,3 +31,19 @@ let solve_string_with_spaces file =
       times distances
   in
   List.fold_left ( * ) 1 solutions
+
+(*solution with the spaces in the input not considered*)
+let solve_string_without_spaces file =
+  let time_and_distance = lines file in
+  let time =
+    List.hd time_and_distance |> String.split_on_char ':' |> List.tl |> List.hd
+    |> Str.global_replace (Str.regexp " ") ""
+    |> float_of_string
+  in
+  let distance =
+    List.hd (List.tl time_and_distance)
+    |> String.split_on_char ':' |> List.tl |> List.hd
+    |> Str.global_replace (Str.regexp " ") ""
+    |> int_of_string |> float_of_int
+  in
+  no_possibilities time distance
